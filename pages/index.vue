dd4fd32804d55874430fca9d2bbf658723baf437
Pascal Mueller <mueller.pascal@hotmail.com>
2021-06-08 00:44:29 +0200

    added basic search

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="blueposts">
+        <input class="input is-large" type="text" placeholder="Search a keyword" v-model="searchString">
         <template
-            v-for="bluepost in blueposts">
+            v-for="bluepost in filteredBlueposts">
 
             <div v-if="bluepost.type == 'qna'" class="block" :key=bluepost.question.id>
                 <div class="is-size-3 question">Q: {{ bluepost.question.content }}</div>
@@ -16,10 +17,40 @@
 
 <script>
 export default {
+  methods: {
+    filterByValue (searchString) {
+      searchString = searchString.toLowerCase()
+      return this.blueposts.filter(post => {
+        if (post.type == "qna") {
+          if (post.question.content.toLowerCase().includes(searchString))
+            return true
+          if (post.answer.content.toLowerCase().includes(searchString))
+            return true
+        } else {
+          if (post.statement.content.toLowerCase().includes(searchString))
+            return true
+        }
+      })
+    }
+  },
   async asyncData ({ $content, params }) {
     const blueposts = await $content('blueposts', params.slug).fetch()
 
     return { blueposts }
+  },
+  data() {
+    return {
+      searchString: '',
+    }
+  },
+  computed: {
+    filteredBlueposts () {
+      if (this.searchString != '') {
+        return this.filterByValue(this.searchString)
+      } else {
+        return this.blueposts
+      }
+    }
   }
 }
 </script>
