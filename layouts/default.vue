3ca93fe77e6beafbaaf45bed9e2a805e8774664c
paliapost <paliapost@users.noreply.github.com>
2021-06-07 23:10:55 +0000

    deploy: 892b97c6157bcf62a1e1ca6939e8da5c842b5134

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <main class="container has-text-weight-bold">
+      <p class="title has-text-centered">Palia Post</p>
+      <nuxt />
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="blueposts">
+        <input class="input is-large" type="text" placeholder="Search a keyword" v-model="searchString">
+        <template
+            v-for="bluepost in filteredBlueposts">
+
+            <div v-if="bluepost.type == 'qna'" class="block" :key=bluepost.question.id>
+                <div class="is-size-3 question">Q: {{ bluepost.question.content }}</div>
+                <div class="is-size-4 answer">A: {{ bluepost.answer.content }}</div>
+            </div>
+            <div v-if="bluepost.type == 'statement'" class="block" :key=bluepost.statement.id>
+              <div class="is-size-3 question">{{ bluepost.statement.content }}</div>
+            </div>
+        </template>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    filterByValue (searchString) {
+      searchString = searchString.toLowerCase()
+      return this.blueposts.filter((post) => {
+        if (post.type === 'qna') {
+          if (post.question.content.toLowerCase().includes(searchString)) {
+            return true
+          }
+          if (post.answer.content.toLowerCase().includes(searchString)) {
+            return true
+          }
+        }
+
+        if (post.type === 'qna') {
+          if (post.statement.content.toLowerCase().includes(searchString)) {
+            return true
+          }
+        }
+
+        return false
+      })
+    }
+  },
+  async asyncData ({ $content, params }) {
+    const blueposts = await $content('blueposts', params.slug).fetch()
+
+    return { blueposts }
+  },
+  data () {
+    return {
+      searchString: ''
+    }
+  },
+  computed: {
+    filteredBlueposts () {
+      if (this.searchString !== '') {
+        return this.filterByValue(this.searchString)
+      } else {
+        return this.blueposts
+      }
+    }
+  }
+}
+</script>
