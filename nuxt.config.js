87ed6aa28eacb36b00f6d6d6744b9911b9b697f1
Pascal Mueller <mueller.pascal@hotmail.com>
2021-06-08 00:45:09 +0200

    changed /dist to /docs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,11 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  
+  generate: {
+    dir: 'docs'
+  },
+
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
