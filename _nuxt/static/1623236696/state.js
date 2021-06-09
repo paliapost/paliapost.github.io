5b83aabbe93b25ef05f3f2842e9a5e22f3d07e17
paliapost <paliapost@users.noreply.github.com>
2021-06-09 11:21:14 +0000

    deploy: 48954d57fc841094abdc8d1b9828d9ea7b099087

--- _nuxt/static/1623236696/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623236696",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f0a81fbc"}}}}(null,"\u002F"));
